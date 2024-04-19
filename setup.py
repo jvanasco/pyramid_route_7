73fe1d9b8a36a766b2d59978ba6f11e8c977a12d
jonathan vanasco <jonathan@2xlp.com>
2024-04-19 17:07:21 -0400

    added py312

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(
